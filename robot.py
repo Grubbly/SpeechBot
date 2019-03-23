439f32565455d4c6b9229165cebf8535e3373162
unknown <orthogonaltrajectory@gmail.com>
2019-03-23 14:39:36 -0800

    Test servo GPIO code for robot

---
+++ robot.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+leftArmPin = 12
+rightArmPin = 13
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(leftArmPin, GPIO.OUT)
+GPIO.setup(rightArmPin, GPIO.OUT)
+
+leftArm = GPIO.PWM(leftArmPin,50)
+rightArm = GPIO.PWM(rightArmPin,51)
+
+leftArm.start(7.5)
+rightArm.start(7,5)
+
+try:
+    while True:
+        leftArm.ChangeDutyCycle(7.5)
+        time.sleep(1)
+        rightArm.ChangeDutyCycle(7.5)
+        time.sleep(1)
+except KeyboardInterrupt:
+        leftArm.stop()
+        rightArm.stop()
+        GPIO.cleanup()
