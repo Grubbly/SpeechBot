7968af445436bf269aa2d69b99be45625b4325fd
Tristan <orthogonaltrajectory@gmail.com>
2019-03-29 13:20:23 -0800

    Left arm moving

--- robot.py
+++ robot.py
@@ -4,23 +4,25 @@ import time
 leftArmPin = 17
 rightArmPin = 18
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(leftArmPin, GPIO.OUT)
-GPIO.setup(rightArmPin, GPIO.OUT)
+# GPIO.setup(rightArmPin, GPIO.OUT)
 
 leftArm = GPIO.PWM(leftArmPin,50)
-rightArm = GPIO.PWM(rightArmPin,51)
+# rightArm = GPIO.PWM(rightArmPin,51)
 
 leftArm.start(7.5)
-rightArm.start(7.5)
+# rightArm.start(7.5)
 
 try:
     while True:
         leftArm.ChangeDutyCycle(7.5)
         time.sleep(1)
-        rightArm.ChangeDutyCycle(7.5)
+        leftArm.ChangeDutyCycle(10)
         time.sleep(1)
+   #     rightArm.ChangeDutyCycle(7.5)
+   #     time.sleep(1)
 except KeyboardInterrupt:
         leftArm.stop()
-        rightArm.stop()
+    #    rightArm.stop()
         GPIO.cleanup()
